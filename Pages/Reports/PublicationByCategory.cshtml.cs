e105de971fd5b683859941f43ba56d1ead1a57af
Laliotis Nikos <81807513+Laliotis21@users.noreply.github.com>
2022-04-11 23:58:49 +0300

    new code

---
+++ Pages/Reports/PublicationByCategory.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ResearchLab_Library.Joins;
+using ResearchLab_Library.Model;
+
+namespace ResearchLab.Pages.Reports
+{
+    public class PublicationByCategoryModel : PageModel
+    {
+        private readonly ResearchLab_Library.LabDbContext _context;
+        
+
+        public PublicationByCategoryModel(ResearchLab_Library.LabDbContext context)
+        {
+            _context = context;
+        }
+
+        public Member Member { get; set; }
+        public Publication Publication { get; set; }   
+        public IList<MembersPublications> MembersPublications { get;set; }
+
+        public async Task OnGetAsync()
+               
+        {
+            MembersPublications = await _context.MembersPublications
+                .Include(x => x.Member).ToListAsync();
+
+            var parameters = new[] {
+                new SqlParameter("@MemberID", MembersPublications),
+                };
+            var Results = _context.Database.ExecuteSqlRaw("exec dbo.ResearchLab_FindPublications @MemberID ", parameters);
+
+        }
+    }
+}
